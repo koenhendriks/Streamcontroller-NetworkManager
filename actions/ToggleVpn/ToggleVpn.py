a4355e29ac9515c217e4c40dc0926532712c44f8
Koen Hendriks <info@koenhendriks.nl>
2024-12-05 22:35:12 +0100

    Added setting up VPN action with working realtime state.

--- actions/ToggleVpn/ToggleVpn.py
+++ actions/ToggleVpn/ToggleVpn.py
@@ -18,10 +18,21 @@ class ToggleVpn(ActionBase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+    def get_selected_vpn_uuid(self) -> str:
+        settings = self.get_settings()
+        vpn = settings.get('vpn_uuid', 'no-selection')
+        return vpn
+
+    def get_selected_vpn_name(self) -> str:
+        settings = self.get_settings()
+        vpn = settings.get('vpn_name', 'no-selection')
+        return vpn
+
     def on_ready(self) -> None:
-        icon_path = os.path.join(self.plugin_base.PATH, "assets", "info.png")
-        self.set_media(media_path=icon_path, size=0.75)
-        self.set_label("yes")
+        self.update()
+
+    def on_tick(self) -> None:
+        self.update()
 
     def on_key_down(self) -> None:
         print("Key down")
@@ -29,23 +40,59 @@ class ToggleVpn(ActionBase):
     def on_key_up(self) -> None:
         print("Key up")
 
-    def get_config_rows(self):
+    def get_config_rows(self) -> "list[Adw.PreferencesRow]":
         vpn_connections = NetworkManager.get_connections('vpn')
         log.debug(f"Found VPN connections: {vpn_connections}")
         rows = [ItemListComboRowListItem("no-selection", "Choose VPN")]
+
         for vpn in vpn_connections:
-            rows.append(
-                ItemListComboRowListItem(vpn.uuid, vpn.name)
-            )
+            rows.append(ItemListComboRowListItem(vpn.uuid, vpn.name))
 
         connection_row = ItemListComboRow(items=rows)
         connection_row.set_title("Select Connection")
+        connection_row.set_selected_item_by_key(self.get_selected_vpn_uuid())
+
         connection_row.connect("notify::selected", self.on_connection_changed)
 
         return [connection_row]
 
     def on_connection_changed(self, widget, *args):
+        log.debug(f"Selected VPN connection: {widget.get_selected_item().name} ({widget.get_selected_item().key})")
+
         settings = self.get_settings()
-        log.debug(f"Selected VPN connection: {widget.get_selected_item().name}")
-        settings["vpn"] = widget.get_selected_item().key
+        settings["vpn_uuid"] = widget.get_selected_item().key
+        settings["vpn_name"] = widget.get_selected_item().name
         self.set_settings(settings)
+
+    def update(self) -> None:
+        vpn_name = self.get_selected_vpn_name()
+        vpn_uuid = self.get_selected_vpn_uuid()
+
+        if vpn_uuid == 'no-selection':
+            return self.handle_no_selection()
+
+        if not NetworkManager.is_connected(vpn_uuid):
+            return self.handle_not_connected(vpn_name)
+
+        return self.handle_connected(vpn_name)
+
+    def handle_no_selection(self):
+        self.set_top_label(text="Select VPN", font_size=13)
+        self.set_center_label("in the")
+        self.set_bottom_label("config")
+
+    def handle_not_connected(self, name:str = ""):
+        self.set_top_label(name)
+        self.set_center_label("")
+        self.set_label("VPN OFF")
+        pass
+
+    def handle_connected(self, name:str = ""):
+        self.set_top_label(name)
+        self.set_center_label("")
+        self.set_label("VPN ON")
+        pass
+
+
+
+
