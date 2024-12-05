8cb7bc3160b2a0467bf931d13e37cec849a4c739
Koen Hendriks <info@koenhendriks.nl>
2024-12-05 20:54:33 +0100

    Initial manifest and naming.

--- main.py
+++ main.py
@@ -13,15 +13,15 @@ class PluginTemplate(PluginBase):
         self.simple_action_holder = ActionHolder(
             plugin_base = self,
             action_base = SimpleAction,
-            action_id = "dev_core447_Template::SimpleAction", # Change this to your own plugin id
+            action_id = "com_koenhendriks_NetworkManager::SimpleAction", # Change this to your own plugin id
             action_name = "Simple Action",
         )
         self.add_action_holder(self.simple_action_holder)
 
         # Register plugin
         self.register(
-            plugin_name = "Template",
-            github_repo = "https://github.com/StreamController/PluginTemplate",
+            plugin_name = "NetworkManager",
+            github_repo = "https://github.com/koenhendriks/Streamcontroller-NetworkManager/",
             plugin_version = "1.0.0",
             app_version = "1.1.1-alpha"
         )
